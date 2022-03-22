d95875e2bb595bf6911672dd756e0a7a0dd71321
Thermiee <navyblack16@yahoo.com>
2022-03-22 20:05:35 +0100

    Add a text input field and a submit button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,7 +8,7 @@ function TodoItem(props) {
        onChange={() => props.handleChangeProps(props.todo.id)}
        /> 
        {props.todo.title}
-       <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+       <button onClick={() => props.deleteTodoProps(props.todo.id)}>
            Delete</button>
        </li>
        
